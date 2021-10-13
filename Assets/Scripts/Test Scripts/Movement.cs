831c4ba4a2bda302a9607cc9fab48852aa278d3a
Zaclaine <zdlane@my.waketech.edu>
2021-10-12 21:36:44 -0400

    Updated Tutorial Level

--- Assets/Scripts/Test Scripts/Movement.cs
+++ Assets/Scripts/Test Scripts/Movement.cs
@@ -7,24 +7,13 @@ public class Movement : MonoBehaviour
 {
     // Source for movement: https://docs.unity3d.com/ScriptReference/Rigidbody.MovePosition.html
 
-    // Movement Properties
     Rigidbody rb;
     public float speed = 5f;
-
-    // Scoring Properties
-    public int score = 0;
-    public Text scoreText;
-
-    public GameObject scoreCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Movement
         rb = GetComponent<Rigidbody>();
-
-        // Score
-        scoreText.text = "Score: " + score.ToString();
     }
 
     // Update is called once per frame
@@ -33,17 +22,4 @@ public class Movement : MonoBehaviour
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         rb.MovePosition(transform.position + input * Time.deltaTime * speed);
     }
-
-    // Scoring implementation
-    public void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Score")
-        {
-            score++;
-            scoreCollider.SetActive(false);
-            scoreText.text = "Score: " + score.ToString();
-        }
-    }
-
-    // This is the primary movement script. I implemented the potential scoring mechanics here as well in the case we wanted to use it.
 }
