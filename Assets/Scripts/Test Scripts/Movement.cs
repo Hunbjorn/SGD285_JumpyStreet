26d42f48e40e2942c3b126d904899b7a982e13db
Zaclaine <zdlane@my.waketech.edu>
2021-10-13 16:15:53 -0400

    Update #2 Tutorial Level
    Fixed dialogue system with collisions.

--- Assets/Scripts/Test Scripts/Movement.cs
+++ Assets/Scripts/Test Scripts/Movement.cs
@@ -9,11 +9,14 @@ public class Movement : MonoBehaviour
 
     Rigidbody rb;
     public float speed = 5f;
+    //public Transform player;
+    private TutorialScript tutorialManager;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        tutorialManager = GameObject.FindGameObjectWithTag("TutorialManager").GetComponent<TutorialScript>();
     }
 
     // Update is called once per frame
@@ -21,5 +24,28 @@ public class Movement : MonoBehaviour
     {
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         rb.MovePosition(transform.position + input * Time.deltaTime * speed);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            // Instantiate(player, new Vector3(0, 1, -4), Quaternion.identity);
+            tutorialManager.InstantiateNewPlayer();
+            tutorialManager.EnemyHit();
+            Destroy(gameObject);
+        }
+
+        if (other.tag == "Prize")
+        {
+            other.gameObject.SetActive(false);
+            tutorialManager.CoinCollected();
+        }
+
+        if(other.tag == "Respawn")
+        {
+            tutorialManager.InstantiateNewPlayer();
+            Destroy(gameObject);
+        }
     }
 }
