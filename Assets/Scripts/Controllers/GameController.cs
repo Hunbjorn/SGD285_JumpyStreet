8fad1bef32861fd2e7117ffc5b9b7502fae552e1
Hunbjorn <89803461+Hunbjorn@users.noreply.github.com>
2021-10-20 17:27:20 -0400

    script changes
    removed terrain script variable and extraneous prize distribution script

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -20,15 +20,7 @@ public class GameController : MonoBehaviour
     public GameObject miscright;
     public GameObject train;
 
-    public GameObject prefab;
-    public Terrain terrain;
     public float yOffset = 0.5f;
-
-    private float terrainWidth;
-    private float terrainLength;
-
-    private float xTerrainPos;
-    private float zTerrainPos;
 
     public GameObject[] gameTiles;
 
@@ -37,16 +29,6 @@ public class GameController : MonoBehaviour
         StartCoroutine(DelayedSpawn());
 
         PlacePrizes();
-
-        //Get terrain size
-        terrainWidth = terrain.terrainData.size.x;
-        terrainLength = terrain.terrainData.size.z;
-
-        //Get terrain position
-        xTerrainPos = terrain.transform.position.x;
-        zTerrainPos = terrain.transform.position.z;
-
-        generateObjectOnTerrain();
     }
 
     void Update() 
@@ -68,20 +50,6 @@ public class GameController : MonoBehaviour
             z = UnityEngine.Random.Range(-6f, 6f);
             return new Vector3(x,y,z);
         }
-    }
-
-    void generateObjectOnTerrain()
-    {
-        //Generate random x,z,y position on the terrain
-        float randX = UnityEngine.Random.Range(xTerrainPos, xTerrainPos + terrainWidth);
-        float randZ = UnityEngine.Random.Range(zTerrainPos, zTerrainPos + terrainLength);
-        float yVal = Terrain.activeTerrain.SampleHeight(new Vector3(randX, 0, randZ));
-
-        //Apply Offset if needed
-        yVal = yVal + yOffset;
-
-        //Generate the Prefab on the generated position
-        GameObject objInstance = (GameObject)Instantiate(prefab, new Vector3(randX, yVal, randZ), Quaternion.identity);
     }
 
     public void VehicleControl()
