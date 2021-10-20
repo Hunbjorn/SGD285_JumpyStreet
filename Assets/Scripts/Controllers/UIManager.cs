3f7a1ceaf57a3dc712b29949d3f621aa399773b0
Zaclaine <zdlane@my.waketech.edu>
2021-10-20 17:04:58 -0400

    Revert "Pause Menu added and extra artwork"
    This reverts commit 02a7e4c87ec606461351065646e0a4981d2cdc01.

--- Assets/Scripts/Controllers/UIManager.cs
+++ Assets/Scripts/Controllers/UIManager.cs
@@ -23,9 +23,6 @@ public class UIManager : MonoBehaviour
     [Header("Game Objects")]
     public GameObject helpPanel;
     public GameObject titlePanel;
-    public GameObject pausePanel;
-
-    private bool isPaused = false;
 
     void Start()
     {
@@ -39,9 +36,6 @@ public class UIManager : MonoBehaviour
             titlePanel.SetActive(false);
             titleBGM.Stop();
         }
-
-        //Pause();
-        //Resume();
     }
 
     //////////////////////////////////////////////////
@@ -59,29 +53,6 @@ public class UIManager : MonoBehaviour
 
     //////////////////////////////////////////////////
 
-    // Pause Menu
-    /*void Pause()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape) && isPaused == false)
-        {
-            isPaused = true;
-            pausePanel.SetActive(true);
-            Time.timeScale = 0;
-        }
-    }
-
-    void Resume()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) && isPaused == true)
-        {
-            isPaused = false;
-            pausePanel.SetActive(false);
-            Time.timeScale = 1;
-        }
-    }*/
-
-    //////////////////////////////////////////////////
-
     // Button Functions
     public void OnStart()
     {
@@ -96,11 +67,6 @@ public class UIManager : MonoBehaviour
     public void OnBack()
     {
         helpPanel.SetActive(false);
-    }
-
-    public void onTutorial()
-    {
-        SceneManager.LoadScene("Tutorial");
     }
 
     public void OnQuit()
